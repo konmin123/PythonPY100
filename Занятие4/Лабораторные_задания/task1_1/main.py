8d3bbb480848698ab92b7542686f6308fe856479
konmin123 <MindlinKS@yandex.ru>
2021-10-27 15:26:23 +0300

    27.10.2021

--- Занятие4/Лабораторные_задания/task1_1/main.py
+++ Занятие4/Лабораторные_задания/task1_1/main.py
@@ -1,3 +1,14 @@
+def quad_esc(n, m):
+    list_ = []
+    n = int(n)
+    m = int(m)
+    for number in range(n, m):
+        if n < number < m:
+            number = number * number
+            list_.append(number)
+        print(list_)
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    quad_esc(2.1, 6.2)
+
