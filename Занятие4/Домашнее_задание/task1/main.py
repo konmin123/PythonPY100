65813e1df59c4676cbc5e80d985cf3f8a6d39369
konmin123 <MindlinKS@yandex.ru>
2021-10-29 15:34:14 +0300

    29.10.2021

--- Занятие4/Домашнее_задание/task1/main.py
+++ Занятие4/Домашнее_задание/task1/main.py
@@ -1,3 +1,13 @@
+def odinakovie(a):
+    list_str = list(str(a))
+    print(list_str)
+    list_digits = list_str
+    for index, i in enumerate(list_digits):
+        list_digits[index] = int(i)
+    for index, i in enumerate(list_digits):
+        if i =
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    odinakovie(456789)
+
